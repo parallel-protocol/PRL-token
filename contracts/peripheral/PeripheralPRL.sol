77ef3ac8665eb7bf196ce65997e3745ec97bb86b
FabienC <fabiencoutant.ctt@gmail.com>
2025-01-29 18:34:46 +0200

    fix: issue_05 - Permit functionality is not necessary in PeripheralPRL

--- contracts/peripheral/PeripheralPRL.sol
+++ contracts/peripheral/PeripheralPRL.sol
@@ -57,37 +57,6 @@ contract PeripheralPRL is OFT, ERC20Permit, Pausable {
         return super.send(_sendParam, _fee, _refundAddress);
     }
 
-    /// @notice Sends tokens to another chain using EIP-2612 permit
-    /// @dev This function combines permit and send operations
-    /// @param _sendParam Parameters for the send operation
-    /// @param _fee Messaging fee for the LayerZero transaction
-    /// @param _refundAddress Address to refund excess fees
-    /// @param _deadline Expiration time of the permit signature
-    /// @param _v ECDSA signature parameter v
-    /// @param _r ECDSA signature parameter r
-    /// @param _s ECDSA signature parameter s
-    /// @return msgReceipt Receipt for the LayerZero message
-    /// @return oftReceipt Receipt for the OFT transaction
-    function sendWithPermit(
-        SendParam calldata _sendParam,
-        MessagingFee calldata _fee,
-        address _refundAddress,
-        uint256 _deadline,
-        uint8 _v,
-        bytes32 _r,
-        bytes32 _s
-    )
-        external
-        payable
-        whenNotPaused
-        returns (MessagingReceipt memory msgReceipt, OFTReceipt memory oftReceipt)
-    {
-        // Approve the spending using EIP-2612 permit
-        // @dev using try catch to avoid reverting the transaction in case of front-running
-        try this.permit(msg.sender, address(this), _sendParam.amount, _deadline, _v, _r, _s) { } catch { }
-        return super.send(_sendParam, _fee, _refundAddress);
-    }
-
     //-------------------------------------------
     // OnlyOwner functions
     //-------------------------------------------
